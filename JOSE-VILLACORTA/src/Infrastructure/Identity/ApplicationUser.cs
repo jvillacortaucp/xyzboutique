8106deb8644fcac9f2412e4d7f04d8e2fd05f843
José Miguel Villacorta Arteaga <jvillacorta@ucp.edu.pe>
2024-07-02 10:45:59 -0500

    Updating Fields For ApplicationUser and Roles
    Actualizando los campos de la entidad ApplicationUser , la cual es utilizada como tabla Usuario. Asimismo, se agrego los roles :
    - Encargado
    - Vendedor
    - Delivery
    - Repartidor (Redundante con Delivery)

--- JOSE-VILLACORTA/src/Infrastructure/Identity/ApplicationUser.cs
+++ JOSE-VILLACORTA/src/Infrastructure/Identity/ApplicationUser.cs
@@ -4,4 +4,12 @@ namespace xyzboutique.Infrastructure.Identity;
 
 public class ApplicationUser : IdentityUser
 {
+    public string? Nombre { get; set; }
+
+    public string? CodigoTrabajador { get; set; }
+
+    public string? Telefono { get; set; }
+
+    public string? Puesto { get; set; }
+
 }
